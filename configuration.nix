09bdb9b49a358b309c4b3c9b1c34b8c3f248d1b9
Bietola <dincio.montesi@gmail.com>
2021-01-16 17:51:52 +0100

    Remove breaking audio fix

--- configuration.nix
+++ configuration.nix
@@ -25,7 +25,7 @@ in
       # Machine-specific configuration
       machineConf.bootLoader
       # TODO: Find why this doesn't work (see todo.md)
-      machineConf.makeSoundWork
+      # machineConf.makeSoundWork
       machineConf.keyboard
       machineConf.networking
 
